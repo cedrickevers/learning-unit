4db038bb743a2659be0ea7498e23728d87ca36cb
Cédric Kévers <32717814+cedrickevers@users.noreply.github.com>
2019-11-25 16:53:20 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,26 +1,47 @@
-<?php 
+<<?php
 namespace App\Models;
 
-class User{
-    
-    public $first_name;
-    public $last_name;
 
-    public function setFirstname($firstname){
-        $this->first_name = $firstname;
-    }
-    public function getFirstName(){
-        return  $this->first_name;
-    }
-    public function setLastName($lastName) {
+class User {
 
-        $this->last_name = $lastName;
-    }
-    public function getLastName() {
-        return $this->last_name;
-    } 
-    
-    public function getFullName() {
-        return "$this->first_name $this->last_name";
-      }
+ public $first_name ;
+ public $last_name;
+ public  $email;
+
+ public function setFirstName($firstName) {
+    $this->first_name = trim($firstName);
+ }
+ public function getFirstName() {
+     return $this->first_name;
+ }
+
+
+ public function setLastName($lastName) {
+    $this->last_name = trim($lastName);
+ }
+ public function getLastName() {
+     return $this->last_name;
+ }
+
+ public function getFullName(){
+
+    return  "$this->first_name $this->last_name";
+ }
+
+ public function setEmail($email){
+     $this->email = $email;
+ }
+ public function getEmail() {
+     return $this->email;
+ }
+
+ public function getEmailVariables(){
+     return [
+         "full_name" => $this->getFullName(),
+         "email" => $this->getEmail()
+     ];
+     
+ }
+
+
 }
