984c93d5e03c3567a081023765f4ad02e55ab6fd
Cédric Kévers <32717814+cedrickevers@users.noreply.github.com>
2019-11-25 16:51:34 +0100

    Create collectionTest.php

---
+++ tests/unit/collectionTest.php
@@ -0,0 +1,14 @@
+<?php
+
+
+/**@test */
+class collectionTest extends \PHPUnit\Framework\TestCase{
+    public function empty_instantiated_collection_returns_no_items(){
+
+        $collecion = new \AppSupporty\collection;
+
+
+    }
+
+
+}
