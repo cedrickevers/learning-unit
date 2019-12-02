c895e85f83c8791dd182150b9548b0d8a75f1150
Cédric Kévers <32717814+cedrickevers@users.noreply.github.com>
2019-12-02 22:58:05 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,47 +1,47 @@
-<<?php
+<?php
+
 namespace App\Models;
 
 
 class User {
 
- public $first_name ;
- public $last_name;
- public  $email;
-
- public function setFirstName($firstName) {
-    $this->first_name = trim($firstName);
- }
- public function getFirstName() {
-     return $this->first_name;
- }
+    public $first_name;
+    public $last_name;
+    public $numbers;
 
 
- public function setLastName($lastName) {
-    $this->last_name = trim($lastName);
- }
- public function getLastName() {
-     return $this->last_name;
- }
 
- public function getFullName(){
+    public function setFirstName($firstName) {
 
-    return  "$this->first_name $this->last_name";
- }
+        $this->first_name = $firstName;
+    }
 
- public function setEmail($email){
-     $this->email = $email;
- }
- public function getEmail() {
-     return $this->email;
- }
+    public function getFirstName(){
+        return $this->first_name;
+    }
 
- public function getEmailVariables(){
-     return [
-         "full_name" => $this->getFullName(),
-         "email" => $this->getEmail()
-     ];
-     
- }
+    public function setLastName($lastName){
+        $this->last_name = $lastName;
+    }
+
+    public function getLastName(){
+        return $this->last_name;
+    }
+
+    public function getFullName(){
+        return "$this->first_name $this->last_name";
+    }
 
 
+    //**************************** */
+
+    public function setAddition( Array $numbers ) {
+
+        $this->numbers = $numbers;
+
+    }
+    
+    public function getAddition(){
+        return array_sum($this->numbers);
+    }
 }
